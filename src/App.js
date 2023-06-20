4a0ad8e0f93c59af02674e2124ee578b85a93431
olahkrisz1 <olahkrisz1@gmail.com>
2023-06-20 17:36:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+const [tasks, setTasks] = useState([
+  {id:1,
+    text: 'Doctors appointment',
+    day: 'feb 5th at 2:30pm',
+    reminder: true,  
+  }
+])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Tasks tasks={tasks}/>
+      
     </div>
   );
 }
